435760b2a217147bb479a8486af162f782e50b22
y_sugiyama_gxp <y.sugiyama@gxp.co.jp>
2024-04-11 14:41:00 +0900

    init

---
+++ RuntimeErrorExample.java
@@ -0,0 +1,6 @@
+public class RuntimeErrorExample {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        System.out.println(numbers[5]);
+    }
+}
---
+++ TypeErrorExample.java
@@ -0,0 +1,6 @@
+public class TypeErrorExample {
+    public static void main(String[] args) {
+        int number = "10";
+        System.out.println(number);
+    }
+}
